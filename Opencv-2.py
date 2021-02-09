89f40e48e4d4417e78df85a2c06f62ef2b9621c0
xx4560xx <78404784+xx4560xx@users.noreply.github.com>
2021-02-09 17:17:40 +0800

    Add files via upload

--- Opencv-2.py
+++ Opencv-2.py
@@ -3,6 +3,18 @@ print('hello')
 import cv2
 print(cv2.__version__)
 capture = cv2.VideoCapture('imop/dog.mp4')
+import numpy as np
+kernel = np.ones((5,5), np.uint8)
+img = cv2.imread('imop/dog.mp4')
+Gaussian = cv2.GaussianBlur(img, (7,7),0)
+Canny = cv2.Canny(img,100, 100)
+
+Dilation = cv2.dilate(Canny, kernel, iterations =1)
+cv2.imshow(dog.mp4',img)
+cv2.imshow('dog.mp4',Gaussian)
+cv2.imshow('dog.mp4 Canny ',Canny)
+cv2.imshow('dog.mp4 dilate',Dilation)
+cv2.waitKey(0)
 
 while True:
    success , img = capture.read()
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-#import cv2
-#print(cv2.__version__)
-#img = cv2.imread('imop/lena.png')
-#cv2.imshow('Lena',img)
-#cv2.waitKey(10000)
+import cv2
+print(cv2.__version__)
+import numpy as np
+kernel = np.ones((5,5), np.uint8)
+img = cv2.imread('imop/lena.png')
+Gaussian = cv2.GaussianBlur(img, (7,7),0)
+Canny = cv2.Canny(img,100, 100)
+
+Dilation = cv2.dilate(Canny, kernel, iterations =1)
+cv2.rectangle(img,(50,100),(350,450),(55,0,0),5)
+cv2.putText(img,'Lena is pretty!',(10,80),2,cv2.FONT_HERSHEY_PLAIN,(255,255,255))
+cv2.imshow('Lena Blur',img)
+cv2.imshow('Lena',Gaussian)
+cv2.imshow('Lena Canny ',Canny)
+cv2.imshow('Lena dilate',Dilation)
+cv2.waitKey(0)
